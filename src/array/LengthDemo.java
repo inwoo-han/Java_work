664b777123fef5b13a8b52c8c7121f31f3bf8b93
inwoo-han <eve9509@nate.com>
2021-04-16 23:24:53 +0900

    04-15 실습

---
+++ src/array/LengthDemo.java
@@ -0,0 +1,18 @@
+package array;
+
+public class LengthDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String[] classGroup = new String[4];
+		classGroup[0] = "한인우";
+		System.out.println(classGroup.length);
+		classGroup[1] = "한인우1";
+		System.out.println(classGroup.length);
+		classGroup[2] = "한인우2";
+		System.out.println(classGroup.length);
+		classGroup[3] = "한인우3";
+		System.out.println(classGroup.length);
+	}
+
+}
