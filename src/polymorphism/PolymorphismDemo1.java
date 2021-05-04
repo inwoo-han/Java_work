7e03bed672dac8e6417070528dc89f5579a35026
DESKTOP-H15P611\User <eve9509@nate.com>
2021-05-04 17:43:00 +0900

    0504

--- src/polymorphism/PolymorphismDemo1.java
+++ src/polymorphism/PolymorphismDemo1.java
@@ -1,14 +1,28 @@
 package polymorphism;
 
-class A{}
+class A{
+	public String x() {
+		return "A.x";
+	}
+}
 
-class B extends A{}
+class B extends A{
+	public String x() {
+		return "B.x";
+	}
+	
+	public String y() {
+		return "B.y";
+	}
+}
 
 public class PolymorphismDemo1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		A obj = new B();
+		System.out.println(obj.x());
+		//obj.y();
 	}
 
 }
